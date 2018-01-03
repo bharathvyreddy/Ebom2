f404b7f3518fc7c5472df9b52262c2db2b4a436d
bharathtest <bharathgus16174@gmail.com>
2018-01-02 23:18:46 -0600

    post test added

--- src/main/java/com/bharth/anvesh/controller/TestController.java
+++ src/main/java/com/bharth/anvesh/controller/TestController.java
@@ -1,7 +1,8 @@
 package com.bharth.anvesh.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bharth.anvesh.model.Circle;
@@ -12,14 +13,8 @@ public class TestController {
 	@Autowired
 	SparePartsService sparePartsService; 
 	
-	Circle circle=new Circle();
-	@RequestMapping("/test")
-	public String test() {
-		circle.x="1";
-		circle.y="2";
-		circle.r="3";
+	@PostMapping("/test")
+	public String test(@RequestBody Circle circle) {
 		return sparePartsService.test(circle);
-		
-		
 	}
 }
